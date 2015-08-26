75bf044cec1048d94093525deb76721d25ff4612
Joseph Schilz <joseph@schilz.org>
2015-08-26 14:04:07 -0700

    Provide writable trait.

---
+++ src/writer/WritableInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jschilz
+ * Date: 8/24/2015
+ * Time: 12:22 PM
+ */
+
+namespace OSFAFramework\Writer;
+
+use OSFAFramework\Visitor\VisitableInterface;
+
+interface WritableInterface extends VisitableInterface {
+
+}
