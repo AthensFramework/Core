87cdb389ef07f581ba7659a6c04aaf48fae9bfe2
Joseph Schilz <joseph@schilz.org>
2015-08-31 11:38:08 -0700

    Add interface for FormAction.

---
+++ src/form/FormActionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UWDOEM\Framework\Form\FormAction;
+
+use UWDOEM\Framework\Writer\WritableInterface;
+
+
+interface FormActionInterface extends WritableInterface {
+
+    function getMethod();
+    
+    function getTarget();
+
+    function getLabel();
+}
