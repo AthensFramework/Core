a0f42e0472912cf923b7811f8ea33c708f528fd6
Joseph Schilz <joseph@schilz.org>
2015-08-31 11:41:27 -0700

    Add writer, tests.

---
+++ src/writer/Writer.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace UWDOEM\Framework\Writer;
+
+use Twig_SimpleFilter;
+
+use UWDOEM\Framework\Field\FieldInterface;
+use UWDOEM\Framework\Form\FormInterface;
+use UWDOEM\Framework\Form\FormAction\FormActionInterface;
+use UWDOEM\Framework\Section\SectionInterface;
+use UWDOEM\Framework\Visitor\Visitor;
+use UWDOEM\Framework\Page\PageInterface;
+use UWDOEM\Framework\Etc\Settings;
+
+
+class Writer extends Visitor {
+
+    protected $_environment;
+
+    protected function getTemplatesDirectories() {
+        $themeDirectories = array_map(
+            function ($theme) {
+                return dirname(__FILE__) . '/templates/' . $theme;
+                },
+            Settings::getTemplateThemes()
+        );
+        return array_merge($themeDirectories, [dirname(__FILE__) . '/templates/base']);
+    }
+
+    protected function write(WritableInterface $host) {
+        return $host->accept($this);
+    }
+
+    protected function getEnvironment() {
+        if (!isset($this->_environment)) {
+            $loader = new \Twig_Loader_Filesystem($this->getTemplatesDirectories());
+            $this->_environment = new \Twig_Environment($loader);
+
+            $filter = new Twig_SimpleFilter('write', function(WritableInterface $host) { return $host->accept($this); });
+            $this->_environment->addFilter($filter);
+        }
+
+        return $this->_environment;
+    }
+
+    protected function loadTemplate($subpath) {
+        return $this->getEnvironment()->loadTemplate($subpath);
+    }
+
+    public function visitSection(SectionInterface $section) {
+        $template = 'section/section.twig';
+
+        return $this
+            ->loadTemplate($template)
+            ->render([
+                "label" => $section->getLabel(),
+                "content" => $section->getContent(),
+                "writables" => $section->getWritables(),
+            ]);
+    }
+
+    public function visitPage(PageInterface $page) {
+        $template = 'page/' . $page->getType() . '.twig';
+
+        $writable = $page->getWritable();
+
+        if ($writable) {
+            $content = $writable->accept($this);
+        } else {
+            $content = "";
+        }
+
+        return $this
+            ->loadTemplate($template)
+            ->render([
+                "title" => $page->getTitle(),
+                "header" => $page->getHeader(),
+                "subHeader" => $page->getSubHeader(),
+                "baseHref" => $page->getBaseHref(),
+                "breadCrumbs" => $page->getBreadCrumbs(),
+                "returnTo" => $page->getReturnTo(),
+                "content" => $content,
+            ]);
+    }
+
+    public function visitField(FieldInterface $field) {
+        $template = 'field/' . $field->getType() . '.twig';
+
+        return $this
+            ->loadTemplate($template)
+            ->render([
+                "slug" => $field->getSlug(),
+                "initial" => $field->getInitial(),
+            ]);
+    }
+
+    public function visitForm(FormInterface $form) {
+        $template = 'form/base.twig';
+
+        return $this
+            ->loadTemplate($template)
+            ->render([
+                "visibleFields" => $form->getFieldBearer()->getVisibleFields(),
+                "hiddenFields" => $form->getFieldBearer()->getHiddenFields(),
+                "actions" => $form->getActions(),
+                "requestURI" => $_SERVER["REQUEST_URI"]
+            ]);
+    }
+
+    public function visitFormAction(FormActionInterface $formAction) {
+        $template = 'form-action/base.twig';
+
+        return $this
+            ->loadTemplate($template)
+            ->render([
+                "label" => $formAction->getLabel(),
+                "method" => $formAction->getMethod(),
+                "target" => $formAction->getTarget(),
+            ]);
+    }
+}
---
+++ tests/WriterTest.php
@@ -0,0 +1,132 @@
+<?php
+
+use UWDOEM\Framework\Field\Field;
+use UWDOEM\Framework\Writer\Writer;
+use UWDOEM\Framework\Form\FormAction\FormAction;
+use UWDOEM\Framework\FieldBearer\FieldBearerBuilder;
+use UWDOEM\Framework\Form\FormBuilder;
+use UWDOEM\Framework\Section\SectionBuilder;
+use UWDOEM\Framework\Page\PageBuilder;
+use UWDOEM\Framework\Page\Page;
+
+
+class WriterTest extends PHPUnit_Framework_TestCase
+{
+    public function testVisitField() {
+        $writer = new Writer();
+
+        /* A literal field */
+        $field = new Field("literal", "A literal field", "initial", true, 200);
+        $this->assertContains("initial", $writer->visitField($field));
+
+        /* A text field */
+        $field = new Field("text", "A text field", "5", true, 200);
+
+        // Get result and strip quotes, for easier analysis
+        $result = str_replace(['"', "'"], "", $writer->visitField($field));
+
+        $this->assertContains('value=5', $result);
+        $this->assertContains('<input type=text', $result);
+    }
+
+    public function testVisitForm() {
+        $writer = new Writer();
+
+        $actions = [
+            new FormAction("JS Action", "JS", "console.log('here');"),
+            new FormAction("POST Action", "POST", "post-target")
+        ];
+        $onValidFunc = function() { return "valid"; };
+        $onInvalidFunc = function() { return "invalid"; };
+
+        $fieldBearer = FieldBearerBuilder::begin()
+            ->setFields([
+                "literalField" => new Field('literal', 'A literal field', 'Literal field content', true),
+                "textField" => new Field('text', 'A text field', "5", false)
+            ])
+            ->build();
+
+        $form = FormBuilder::begin()
+            ->setActions($actions)
+            ->setFieldBearer($fieldBearer)
+            ->setOnInvalidFunc($onInvalidFunc)
+            ->setOnValidFunc($onValidFunc)
+            ->build();
+
+        $_SERVER["REQUEST_URI"] = "http://example.com";
+
+        // Get result and strip quotes, for easier analysis
+        $result = str_replace(['"', "'"], "", $writer->visitForm($form));
+
+        $this->assertContains("<form", $result);
+        $this->assertContains("data-request-uri=http://example.com", $result);
+        $this->assertContains("data-for=a-literal-field", $result);
+        $this->assertContains("A literal field*:", $result);
+        $this->assertContains("Literal field content", $result);
+        $this->assertContains("data-for=a-text-field", $result);
+        $this->assertContains("A text field:", $result);
+        $this->assertContains("value=5", $result);
+        $this->assertContains("name=a-text-field", $result);
+        $this->assertContains('<input type=text', $result);
+        $this->assertContains('onclick=console.log(here);', $result);
+        $this->assertContains('JS Action</button>', $result);
+        $this->assertContains('<input class=form-action type=submit', $result);
+        $this->assertContains('value=POST Action', $result);
+        $this->assertContains('</form>', $result);
+    }
+
+    public function testVisitSection() {
+        $writer = new Writer();
+
+        $subSection = SectionBuilder::begin()
+            ->setContent("Some sub-content.")
+            ->build();
+
+        $section = SectionBuilder::begin()
+            ->setLabel("Label")
+            ->setContent("Some content.")
+            ->setWritables([$subSection])
+            ->build();
+
+        // Get result and strip quotes, for easier analysis
+        $result = str_replace(['"', "'"], "", $writer->visitSection($section));
+
+        $this->assertContains("<div class=section-label>Label</div>", $result);
+        $this->assertContains("<div class=section-writables>", $result);
+        $this->assertContains("Some sub-content.", $result);
+    }
+
+    public function testVisitPage() {
+        $writer = new Writer();
+
+        $section = SectionBuilder::begin()
+            ->setLabel("Label")
+            ->setContent("Some content.")
+            ->build();
+
+        $page = PageBuilder::begin()
+            ->setWritable($section)
+            ->setHeader("Page Header")
+            ->setSubHeader("Page subheader")
+            ->setReturnTo(["Another name" => "http://another.link"])
+            ->setType(Page::PAGE_TYPE_FULL_HEADER)
+            ->setTitle("Page Title")
+            ->setBaseHref(".")
+            ->setBreadCrumbs(["Key" => "http://example.com", "Another name"])
+            ->build();
+
+        // Get result and strip quotes, for easier analysis
+        $result = str_replace(['"', "'"], "", $writer->visitPage($page));
+
+        $this->assertContains("<html>", $result);
+        $this->assertContains("<head>", $result);
+        $this->assertContains("<title>Page Title</title>", $result);
+        $this->assertContains("<base href=.", $result);
+        $this->assertContains("</head>", $result);
+        $this->assertContains("<body>", $result);
+        $this->assertContains("<li><a target=_self href=http://example.com>Key</a></li>", $result);
+        $this->assertContains("<h1 class=header>Page Header</h1>", $result);
+        $this->assertContains("<h2 class=subheader>Page subheader</h2>", $result);
+        $this->assertContains("<div class=section-label>Label</div>", $result);
+    }
+}
