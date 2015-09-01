1128015876ab341513619c0c9c96baa233bd1fcd
Joseph Schilz <joseph@schilz.org>
2015-09-01 13:33:30 -0700

    Add ORMUtils, tests.

---
+++ src/etc/ORMUtils.php
@@ -0,0 +1,338 @@
+<?php
+
+namespace UWDOEM\Framework\Etc;
+
+use UWDOEM\Framework\Field\Field;
+use Propel\Runtime\Map\Exception\ColumnNotFoundException;
+
+
+class ORMUtils {
+
+    static protected $db_type_to_field_type_association = [
+        "VARCHAR"=> "text",
+        "INTEGER"=> "text",
+        "VARBINARY"=> "text",
+        "TIMESTAMP"=> "datetime",
+        "BOOLEAN"=> "yorn",
+        "FLOAT"=> "text",
+    ];
+
+    /**
+     * @param $object
+     * @return Field[]
+     */
+    static public function makeFieldsFromObject($object) {
+        $fieldNames = static::makeFieldNamesFromObject($object);
+
+        $columns = array_combine($fieldNames, static::getColumns($object::TABLE_MAP));
+        $fields = array_combine($fieldNames, static::makeFieldsFromColumns($columns));
+
+        foreach($columns as $fieldName=>$column) {
+            $phpName = $column->getPhpName();
+            $initial = $object->{"get" . $phpName}();
+
+            $fields[$fieldName]->setInitial($initial);
+        }
+
+        return $fields;
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return \Propel\Runtime\ActiveRecord\ActiveRecordInterface
+     */
+    static public function makeNewObjectFromClassTableMapName($classTableMapName) {
+        $className = static::getObjectClass($classTableMapName);
+        return new $className;
+    }
+
+    /**
+     * @param string $fieldName
+     * @param string $classTableMapName
+     * @return bool
+     */
+    static public function isEncrypted($fieldName, $classTableMapName) {
+        if (defined($classTableMapName . "::ENCRYPTED_COLUMNS")) {
+
+            $encryptedFieldNames = explode(" ", $classTableMapName::ENCRYPTED_COLUMNS);
+            $phpName = explode(".", $fieldName)[1];
+
+            return array_search($phpName, $encryptedFieldNames) !== false;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $object
+     * @return string[]
+     */
+    static protected function makeFieldNamesFromObject($object) {
+        $objectName = static::getPhpNameFromObject($object);
+        $columns = static::getColumns($object::TABLE_MAP);
+
+        return array_values(array_map(function($column) use ($objectName) {
+            return $objectName . "." . $column->getPhpName();
+        }, $columns));
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return \Propel\Runtime\Map\TableMap
+     */
+    static protected function getClassTableMap($classTableMapName) {
+        return $classTableMapName::getTableMap();
+    }
+
+    /**
+     * Return a Propel TableMap corresponding to a table within the same schema as $fullyQualifiedTableMapName.
+     *
+     * In some cases, a foreign table map within the same database as $this may not be initialized by Propel. If
+     * we try to access a foregin table map using runtime introspection and it has not yet been initialized, then
+     * Propel will throw a TableNotFoundException. This method accesses the table map by access to its fully
+     * qualified class name, which it determines by modifying $this->_classTableMapName.
+     *
+     * @param   string    $tableName  The SQL name of the related table for which you would like to retrieve a table map
+     * @param   string    $fullyQualifiedTableMapName  A fully qualified table map name within the schema of
+     *
+     * @return  \Propel\Runtime\Map\TableMap
+     */
+    static protected function getRelatedTableMap($tableName, $fullyQualifiedTableMapName) {
+
+        $upperCamelCaseTableName = StringUtils::toUpperCamelCase($tableName) . "TableMap";
+
+        // We build he fully qualified name of the related table map class
+        // by doing some complicated search and replace on the fully qualified
+        // table map name of the child.
+        $fullyQualifiedRelatedTableName = substr_replace(
+                $fullyQualifiedTableMapName,
+                $upperCamelCaseTableName, strrpos($fullyQualifiedTableMapName, "\\", -1) + 1) . "\n";
+        $fullyQualifiedParentTableName = trim($fullyQualifiedRelatedTableName);
+
+        return static::getClassTableMap($fullyQualifiedParentTableName);
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return \Propel\Runtime\Map\TableMap[]
+     */
+    static protected function findParentTables($classTableMapName) {
+        // Make recursive
+        $behaviors = static::getClassTableMap($classTableMapName)->getBehaviors();
+
+        $parentTables = [];
+        if (array_key_exists("delegate", $behaviors)) {
+            $parentTables[] = static::getRelatedTableMap($behaviors["delegate"]["to"], $classTableMapName);
+        }
+        return $parentTables;
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return \Propel\Runtime\Map\ColumnMap[]
+     */
+    static protected function getColumns($classTableMapName) {
+        $columns = [];
+
+        foreach (static::findParentTables($classTableMapName) as $parentTable) {
+            $columns = array_merge($columns, $parentTable->getColumns());
+        }
+
+        return array_merge($columns, static::getClassTableMap($classTableMapName)->getColumns());
+    }
+
+    /**
+     * @param \Propel\Runtime\Map\ColumnMap $column
+     * @return string
+     */
+    static protected function chooseFieldType($column) {
+
+        $type = ORMUtils::$db_type_to_field_type_association[$column->getType()];
+
+        if ($type == "text" && $column->getSize() >= 128) {
+            $type = "textarea";
+        }
+
+        return $type;
+    }
+
+    /**
+     * @param \Propel\Runtime\Map\ColumnMap[] $columns
+     * @return Field[]
+     */
+    static protected function makeFieldsFromColumns(array $columns) {
+
+        $fields = [];
+        $initial = "";
+        foreach ($columns as $column) {
+            $label = $column->getName();
+
+            // The primary key ID field should be presented as a hidden html field
+            if ($label == 'id') {
+                $fieldType = "hidden";
+                $fieldRequired = False;
+            } elseif ($column->isForeignKey()) {
+                $fieldType = "foreignkey";
+                $fieldRequired = False;
+            } else {
+                $fieldType = self::chooseFieldType($column);
+                $fieldRequired = $column->isNotNull();
+            }
+
+            $label = StringUtils::toTitleCase($label);
+
+            $fieldSize = $column->getSize();
+
+            $fields[] = new Field($fieldType, $label, $initial, $fieldRequired, $fieldSize);
+        }
+
+        return $fields;
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return \UWDOEM\Framework\Field\Field[]
+     */
+    static protected function makeFieldsFromClassTableMapName($classTableMapName) {
+        $columns = static::getColumns($classTableMapName);
+        return static::makeFieldsFromColumns($columns);
+    }
+
+    /**
+     * @param $object
+     * @return string
+     */
+    static protected function getPhpNameFromObject($object) {
+        $tableMapName = $object::TABLE_MAP;
+
+        /** @var \Propel\Runtime\Map\TableMap $tableMap */
+        $tableMap = new $tableMapName();
+        return $tableMap->getPhpName();
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return string
+     */
+    static protected function getObjectClass($classTableMapName) {
+        $search = ["\\Map", "TableMap"];
+        $replace = ["", ""];
+
+        return str_replace($search, $replace, $classTableMapName);
+    }
+
+    /**
+     * Return a Propel ActiveQuery object corresponding to $this->_classTableMapName
+     *
+     * @return \Propel\Runtime\ActiveQuery\PropelQuery
+     */
+    static protected function createQuery($classTableMapName) {
+
+        $queryName = str_replace(["TableMap", "\\Map\\"], ["Query", "\\"], $classTableMapName);
+        return $queryName::create();
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @param string $fieldPhpName
+     * @param mixed $value
+     * @param "find"|"findOne" $findType
+     * @return \Propel\Runtime\ActiveRecord\ActiveRecordInterface[]|\Propel\Runtime\ActiveRecord\ActiveRecordInterface
+     */
+    static protected function baseFindAmongInheritance($classTableMapName, $fieldPhpName, $value, $findType) {
+        // If the field is native to this class...
+        try {
+            // Test that the table has the prescribed column. Will fail with a ColumnNotFoundException if it does not.
+            $unused = static::getClassTableMap($classTableMapName)->getColumnByPhpName($fieldPhpName);
+
+            return static::createQuery($classTableMapName)->
+                        {"filterBy" . $fieldPhpName}($value)->
+                        $findType();
+
+            // Else, the field is native to a parent class (we hope!)...
+        } catch (ColumnNotFoundException $e) {
+            // Make recursive. Include error for when we cannot find the field.
+
+            return static::createQuery($classTableMapName)->
+                        {"use" . static::findParentTables($classTableMapName)[0]->getPhpName() . "Query"}()->
+                        {"filterBy" . $fieldPhpName}($value)->
+                        endUse()->
+                        $findType();
+        }
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @param string $fieldPhpName
+     * @param mixed $value
+     * @return \Propel\Runtime\ActiveRecord\ActiveRecordInterface[]
+     */
+    static protected function findByAmongInheritance($classTableMapName, $fieldPhpName, $value) {
+        return static::baseFindAmongInheritance($classTableMapName, $fieldPhpName, $value, "find");
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @param string $fieldPhpName
+     * @param mixed $value
+     * @return \Propel\Runtime\ActiveRecord\ActiveRecordInterface[]
+     */
+    static protected function findOneByAmongInheritance($classTableMapName, $fieldPhpName, $value) {
+        return static::baseFindAmongInheritance($classTableMapName, $fieldPhpName, $value, "findOne");
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @param int $id
+     * @return \Propel\Runtime\ActiveRecord\ActiveRecordInterface
+     */
+    static protected function getObjectById($classTableMapName, $id) {
+        return static::createQuery($classTableMapName)->findPk($id);
+    }
+
+    /**
+     * @param $classTableMapName
+     * @return string
+     */
+    static protected function getTableName($classTableMapName) {
+        $columns = static::getColumns($classTableMapName);
+        return array_values($columns)[0]->getTableName();
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @param string $columnName
+     * @return bool
+     */
+    static protected function isColumnOf($classTableMapName, $columnName) {
+        $columns = static::getColumns($classTableMapName);
+        return array_key_exists($columnName, $columns);
+    }
+
+    /**
+     * @param string $classTableMapName
+     * @return null|\Propel\Runtime\ActiveRecord\ActiveRecordInterface
+     */
+    static protected function getObjectFromURL($classTableMapName) {
+        $object = null;
+
+        $urlIdKey = static::getTableName($classTableMapName) . "_id";
+
+        // Test whether the primary key of the object is in the GET vars
+        if (array_key_exists($urlIdKey, $_GET)) {
+            $object = static::getObjectById($classTableMapName, $_GET[$urlIdKey]);
+        } else {
+        // Else, test whether a related id is in the GET vars
+            foreach ($_GET as $key=>$value) {
+                if (static::isColumnOf($classTableMapName, strtoupper($key))) {
+                    $fieldPhpName = StringUtils::toUpperCamelCase($key);
+                    $object = static::findOneByAmongInheritance($classTableMapName, $fieldPhpName, $value);
+
+                    break;
+                }
+            }
+        }
+        return $object;
+    }
+
+}
---
+++ tests/ORMUtilsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use UWDOEM\Framework\Etc\ORMUtils;
+use UWDOEMTest\TestClass;
+use UWDOEM\Encryption\Cipher;
+
+Cipher::createInstance("my_secret_passphrase");
+
+
+class ORMUtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testMakeFieldNamesFromObject() {
+        $o = new TestClass();
+
+        $fields = ORMUtils::makeFieldsFromObject($o);
+        $fieldNames = array_keys($fields);
+
+        $this->assertContains("TestClass.Id", $fieldNames);
+        $this->assertContains("TestClass.FieldSmallVarchar", $fieldNames);
+        $this->assertContains("TestClass.FieldLargeVarchar", $fieldNames);
+        $this->assertContains("TestClass.FieldInteger", $fieldNames);
+        $this->assertContains("TestClass.FieldFloat", $fieldNames);
+        $this->assertContains("TestClass.FieldTimestamp", $fieldNames);
+        $this->assertContains("TestClass.FieldBoolean", $fieldNames);
+        $this->assertContains("TestClass.RequiredField", $fieldNames);
+        $this->assertContains("TestClass.UnrequiredField", $fieldNames);
+        $this->assertContains("TestClass.EncryptedField", $fieldNames);
+
+        $this->assertEquals(10, sizeof($fieldNames));
+    }
+
+    public function testMakeFieldsFromObject() {
+        $o = new TestClass();
+
+        $fields = ORMUtils::makeFieldsFromObject($o);
+
+        // Test that the inferred field types are correct
+        $this->assertEquals("hidden", $fields["TestClass.Id"]->getType());
+        $this->assertEquals("text", $fields["TestClass.FieldSmallVarchar"]->getType());
+        $this->assertEquals("textarea", $fields["TestClass.FieldLargeVarchar"]->getType());
+        $this->assertEquals("text", $fields["TestClass.FieldInteger"]->getType());
+        $this->assertEquals("text", $fields["TestClass.FieldFloat"]->getType());
+        $this->assertEquals("datetime", $fields["TestClass.FieldTimestamp"]->getType());
+        $this->assertEquals("yorn", $fields["TestClass.FieldBoolean"]->getType());
+
+        // Test that requirement inference is correct
+        $this->assertTrue($fields["TestClass.RequiredField"]->isRequired());
+        $this->assertFalse($fields["TestClass.UnrequiredField"]->isRequired());
+
+        // Test that encryption inference is correct
+        $this->assertTrue(ORMUtils::isEncrypted("TestClass.EncryptedField", $o::TABLE_MAP));
+        $this->assertFalse(ORMUtils::isEncrypted("TestClass.FieldSmallVarchar", $o::TABLE_MAP));
+    }
+}
