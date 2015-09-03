e544d2d34931d68996258578fad00b8f92f37fbd
Joseph Schilz <joseph@schilz.org>
2015-09-03 14:40:26 -0700

    Adjust test to new settings.

--- tests/SettingsTest.php
+++ tests/SettingsTest.php
@@ -15,12 +15,14 @@ class SettingsTest extends PHPUnit_Framework_TestCase {
     public function testAddTemplateTheme() {
         MockSettings::addTemplateTheme("theme1");
         MockSettings::addTemplateTheme("theme2");
+        MockSettings::addTemplateDirectory("/path/to/template/theme");
 
         $result = MockSettings::getTemplateDirectories();
 
         $this->assertContains("theme1", $result[0]);
         $this->assertContains("theme2", $result[1]);
-        $this->assertEquals(2, sizeof($result));
+        $this->assertContains("/path/to/template/theme", $result[2]);
+        $this->assertEquals(3, sizeof($result));
 
         MockSettings::clear();
     }
