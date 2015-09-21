3a2bdfae2c43cc38c2ff67216ebf2fa88150774e
Joseph Schilz <joseph@schilz.org>
2015-09-21 15:20:25 -0700

    Add FilterBuilder, tests.

---
+++ src/filter/FilterBuilder.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace UWDOEM\Framework\Filter;
+
+use UWDOEM\Framework\Etc\Settings;
+
+
+class FilterBuilder {
+
+    /** @var string */
+    protected $_type;
+
+    /** @var int */
+    protected $_paginateBy;
+
+    /** @var string */
+    protected $_fieldName;
+
+    /** @var string */
+    protected $_condition;
+
+    /** @var mixed */
+    protected $_criterion;
+
+    /** @var string */
+    protected $_handle;
+
+
+    protected function __construct() {}
+
+    public static function begin() {
+        return new static();
+    }
+
+    /**
+     * @param string $type
+     * @return FilterBuilder
+     */
+    public function setType($type) {
+        $this->_type = $type;
+        return $this;
+    }
+
+    /**
+     * @param int $paginateBy
+     * @return FilterBuilder
+     */
+    public function setPaginateBy($paginateBy) {
+        $this->_paginateBy = $paginateBy;
+        return $this;
+    }
+
+    /**
+     * @param string $condition
+     * @return FilterBuilder
+     */
+    public function setCondition($condition) {
+        $this->_condition = $condition;
+        return $this;
+    }
+
+    /**
+     * @param mixed $criterion
+     * @return FilterBuilder
+     */
+    public function setCriterion($criterion) {
+        $this->_criterion = $criterion;
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return FilterBuilder
+     */
+    public function setHandle($name) {
+        $this->_handle = $name;
+        return $this;
+    }
+
+    /**
+     * @param string $fieldName
+     * @return FilterBuilder
+     */
+    public function setFieldName($fieldName) {
+        $this->_fieldName = $fieldName;
+        return $this;
+    }
+
+    /**
+     * If it has been set, retrieve the indicated property from this builder. If not, throw exception.
+     *
+     * @param string $attrName The name of the attribute to retrieve, including underscore.
+     * @param string $methodName The name of the calling method, optional.
+     * @param string $reason An optional, additional "reason" to display with the exception.
+     * @return mixed The indicated attribute, if set.
+     * @throws \Exception if the indicated attribute has not been set, or if the attribute does not exist
+     */
+    protected function retrieveOrException($attrName, $methodName = "this method", $reason = "") {
+
+        if (!property_exists($this, $attrName)) {
+            throw new \Exception("Property $attrName not found in class.");
+        }
+
+        if (!isset($this->$attrName)) {
+            $message = $reason ? "Becase you $reason, " : "You ";
+            $message .= "must set $attrName for this object before calling $methodName.";
+
+            throw new \Exception($message);
+        }
+
+        return $this->$attrName;
+    }
+
+    /**
+     * @return FilterInterface
+     * @throws \Exception if an appropriate combination of fields have not been set.
+     */
+    public function build() {
+
+        $handle = $this->retrieveOrException("_handle", __METHOD__);
+        $type = $this->retrieveOrException("_type", __METHOD__);
+
+        $statements = [];
+        switch ($type) {
+            case Filter::TYPE_STATIC:
+
+                $fieldName = $this->retrieveOrException("_fieldName", __METHOD__);
+                $condition = $this->retrieveOrException("_condition", __METHOD__);
+                $criterion = $this->retrieveOrException("_criterion", __METHOD__);
+
+                $statements[] = new FilterStatement($fieldName, $condition, $criterion);
+
+                break;
+            case Filter::TYPE_PAGINATION:
+
+                $paginateBy = isset($this->_paginateBy) ? $this->_paginateBy : Settings::getDefaultPagination();
+
+                $statements[] = new FilterStatement(null, FilterStatement::COND_PAGINATE_BY, $paginateBy);
+
+                break;
+        }
+
+        if (empty($statements)) {
+            throw new \Exception("Invalid filter type.");
+        }
+
+        return new Filter($type, $statements);
+    }
+
+
+
+}
--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -2,19 +2,114 @@
 
 use UWDOEM\Framework\Filter\FilterStatement;
 
+use UWDOEM\Framework\Filter\FilterBuilder;
+use UWDOEM\Framework\Filter\Filter;
+use UWDOEM\Framework\Etc\Settings;
+
 
 class FilterTest extends PHPUnit_Framework_TestCase {
+
+    protected $conditions = [
+        FilterStatement::COND_SORT_ASC,
+        FilterStatement::COND_SORT_DESC,
+        FilterStatement::COND_LESS_THAN,
+        FilterStatement::COND_GREATER_THAN,
+        FilterStatement::COND_EQUAL_TO,
+        FilterStatement::COND_NOT_EQUAL_TO,
+        FilterStatement::COND_PAGINATE_BY,
+        FilterStatement::COND_TRUTHY,
+        FilterStatement::COND_FALSEY,
+    ];
+
 
     public function testFilterStatement() {
         $fieldName = (string)rand();
         $condition = (string)rand();
         $criterion = (string)rand();
 
-        $filterStatement = new FilterStatement($fieldName, $condition, $criterion);
+        $statement = new FilterStatement($fieldName, $condition, $criterion);
 
-        $this->assertEquals($fieldName, $filterStatement->getFieldName());
-        $this->assertEquals($condition, $filterStatement->getCondition());
-        $this->assertEquals($criterion, $filterStatement->getCriterion());
+        $this->assertEquals($fieldName, $statement->getFieldName());
+        $this->assertEquals($condition, $statement->getCondition());
+        $this->assertEquals($criterion, $statement->getCriterion());
+    }
+
+    public function testBuildStaticFilter() {
+
+        $fieldName = (string)rand();
+        $condition = array_rand(array_flip($this->conditions), 1);
+        $criterion = (string)rand();
+        $handle = (string)rand();
+
+        $filter = FilterBuilder::begin()
+            ->setType(Filter::TYPE_STATIC)
+            ->setHandle($handle)
+            ->setFieldName($fieldName)
+            ->setCondition($condition)
+            ->setCriterion($criterion)
+            ->build();
+
+        $this->assertEquals(1, sizeof($filter->getStatements()));
+
+        $statement = $filter->getStatements()[0];
+
+        $this->assertEquals($fieldName, $statement->getFieldName());
+        $this->assertEquals($condition, $statement->getCondition());
+        $this->assertEquals($criterion, $statement->getCriterion());
+    }
+
+    public function testBuildPaginationFilter() {
+        $paginateBy = rand();
+        $handle = (string)rand();
+
+        $filter = FilterBuilder::begin()
+            ->setType(Filter::TYPE_PAGINATION)
+            ->setHandle($handle)
+            ->setPaginateBy($paginateBy)
+            ->build();
+
+        $this->assertEquals(1, sizeof($filter->getStatements()));
+
+        $statement = $filter->getStatements()[0];
+
+        $this->assertEquals(FilterStatement::COND_PAGINATE_BY, $statement->getCondition());
+        $this->assertEquals($paginateBy, $statement->getCriterion());
+    }
+
+    public function testBuildPaginationFilterUsesPaginateSetting() {
+        $paginateBy = rand();
+        $handle = (string)rand();
+
+        // Store the current default pagination
+        $defaultPagination = Settings::getDefaultPagination();
+
+        // Set our new pagination
+        Settings::setDefaultPagination($paginateBy);
+
+        $filter = FilterBuilder::begin()
+            ->setType(Filter::TYPE_PAGINATION)
+            ->setHandle($handle)
+            ->build();
+
+        $this->assertEquals(1, sizeof($filter->getStatements()));
+
+        $statement = $filter->getStatements()[0];
+
+        $this->assertEquals(FilterStatement::COND_PAGINATE_BY, $statement->getCondition());
+        $this->assertEquals($paginateBy, $statement->getCriterion());
+
+        // Return the old default pagination
+        Settings::setDefaultPagination($defaultPagination);
+    }
+
+    /**
+     * @expectedException              Exception
+     * @expectedExceptionMessageRegExp #You must set _handle.*#
+     */
+    public function testBuildFilterErrorWithoutHandle() {
+        $filter = FilterBuilder::begin()
+            ->setType(Filter::TYPE_PAGINATION)
+            ->build();
     }
 
 }
