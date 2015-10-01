368cc5e061e95c1c8a77b5b5540d7abef825e4a5
Joseph Schilz <joseph@schilz.org>
2015-10-01 07:29:57 -0700

    Refactor functions for controlling pagination section.

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,22 +1,86 @@
-
-
 uwdoem.pagination = (function() {
 
-    var setupPaginationFilter = function(marker) {
+    var getPaginationContainer = function(handle) {
+        return $("div.pagination-container[data-handle-for=" + handle +"]");
+    };
+
+    var getFilterSectionName = function(handle) {
+        return getPaginationContainer(handle).closest('.ajax-loaded-section').data('section-name');
+    };
+
+    var getMaxPages = function(handle) {
+        return getPaginationContainer().find("option").last().html();
+    };
+
+    var setArrows = function(handle, page) {
+        var paginationContainer, maxPages;
+
+        paginationContainer = getPaginationContainer(handle);
+        maxPages = getMaxPages(handle);
+
+        if (page === 1) {
+            paginationContainer.find("a.pagination-arrow.back").css("display", "none");
+        } else {
+            paginationContainer.find("a.pagination-arrow.previous").attr("data-page-for", page - 1);
+        }
+
+        if (page === maxPages) {
+            paginationContainer.find("a.pagination-arrow.forward").css("display", "none");
+        } else {
+            paginationContainer.find("a.pagination-arrow.next").attr("data-page-for", page + 1);
+        }
+    };
+
+    var setSelect = function(handle, page) {
+        getPaginationContainer().find("select").val(page);
+    };
+
+    var setControls = function(handle, page) {
+        setArrows(handle, page);
+        setSelect(handle, page);
+    };
+
+    var registerPage = function(handle, page) {
+        var filterSectionName, getVar;
+
+        filterSectionName = getFilterSectionName(handle);
+        getVar = uwdoem.ajax_section.getVar(filterSectionName, handle, "page", page);
+
+        uwdoem.ajax_section.registerGetVar(getVar);
+    };
+
+    var getPage = function(handle) {
+        var filterSectionName = getFilterSectionName(handle);
+        return uwdoem.ajax_section.getGetVarValue(filterSectionName, 'pagination', 'page').value || 1;
+    };
+
+    var setupPaginationFilter = function(handle) {
         $(function() {
+            var paginationContainer, page, filterSectionName;
+
+            paginationContainer = getPaginationContainer(handle);
+
+            paginationContainer.appendTo(paginationContainer.closest("div.section-container").find("div.section-label"));
+            page = getPage(handle);
+
+            setControls(handle, page);
+
             $("div.pagination-container a.pagination-arrow").click(function() {
-                var filterSectionName=$(this).parents('.ajax-loaded-section').data('section-name');
                 var targetPage = $(this).attr('data-page-for');
-                uwdoem.ajax_section.registerGetVar(filterSectionName, 'pagination', 'page', targetPage);
+                uwdoem.ajax_section.registerGetVar(filterSectionName, handle, 'page', targetPage);
                 uwdoem.ajax_section.loadSection(filterSectionName);
+
+                registerPage(targetPage);
+                setControls(handle, page);
 
                 return false;
             });
 
-            $("select.pagination-filter." + marker).change(function() {
-                var filterSectionName = $(this).parents('.ajax-loaded-section').data('section-name');
-                uwdoem.ajax_section.registerGetVar(filterSectionName, 'pagination', 'page', $( "select.pagination-filter." + marker + " option:selected" ).text());
-                uwdoem.ajax_section.loadSection(filterSectionName);
+            $("select.pagination-filter." + handle).change(function() {
+                var targetPage = $( "select.pagination-filter." + handle + " option:selected" ).text();
+
+                registerPage(targetPage);
+                setControls(handle, page);
             });
         });
     };
