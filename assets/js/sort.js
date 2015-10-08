8c19871c71dfec618bffd5eb8b1b5c5b3989c09c
Joseph Schilz <joseph@schilz.org>
2015-10-08 13:20:17 -0700

    Fix sort.js.

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -4,29 +4,32 @@ uwdoem.sort = (function() {
     
     var setupSortFilter = function (handle) {
         var filterSection = $("div.sort-container[data-handle-for=" + handle + "]").parents('.ajax-loaded-section');
-        var filterSectionName=filterSection.data('section-name');
+        var filterSectionName = filterSection.data('section-name');
+        var ajaxSectionName = filterSection.closest("div.ajax-loaded-section").attr("id");
 
         var headers = filterSection.find("th");
         headers.addClass("clickable");
         headers.removeClass("sorted ascending descending");
 
-        var fieldname = uwdoem.ajax_section.getGetVar(filterSectionName, 'sort', 'fieldname');
-        var order = uwdoem.ajax_section.getGetVar(filterSectionName, 'sort', 'order');
+        var fieldname = uwdoem.ajax_section.getGetVarValue(ajaxSectionName, handle, 'fieldname');
+        var order = uwdoem.ajax_section.getGetVarValue(ajaxSectionName, handle, 'order');
         filterSection.find("th[data-header-for='" + fieldname + "']").addClass("sorted " + order);
 
         headers.click(function() {
             var fieldname = $(this).attr("data-header-for");
-            uwdoem.ajax_section.registerGetVar(filterSectionName, 'sort', 'fieldname', fieldname);
 
-            var oldOrder = uwdoem.ajax_section.getGetVar(filterSectionName, 'sort', 'order');
-            var oldFieldname = uwdoem.ajax_section.getGetVar(filterSectionName, 'sort', 'fieldname');
+            var oldOrder = uwdoem.ajax_section.getGetVarValue(ajaxSectionName, handle, 'order');
+            var oldFieldname = uwdoem.ajax_section.getGetVarValue(ajaxSectionName, handle, 'fieldname');
 
             var newOrder = "ascending";
             if (fieldname === oldFieldname && oldOrder === "ascending") {
-                newOrder = "ascending";
+                newOrder = "descending";
             }
-            uwdoem.ajax_section.registerGetVar(filterSectionName, 'sort', 'order', newOrder);
-            uwdoem.ajax_section.loadSection(filterSectionName);
+
+            uwdoem.ajax_section.registerGetVar(uwdoem.ajax_section.getVar(ajaxSectionName, handle, 'fieldname', fieldname));
+            uwdoem.ajax_section.registerGetVar(uwdoem.ajax_section.getVar(ajaxSectionName, handle, 'order', newOrder));
+
+            uwdoem.ajax_section.loadSection(ajaxSectionName);
         });
     };
 
