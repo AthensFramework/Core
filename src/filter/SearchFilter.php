f1c0c91960cad4c3f5b3c66a6cbccb0c48074196
Joseph Schilz <joseph@schilz.org>
2015-10-09 14:32:57 -0700

    Add feedback to search filter.

--- src/filter/SearchFilter.php
+++ src/filter/SearchFilter.php
@@ -2,8 +2,41 @@
 
 namespace UWDOEM\Framework\Filter;
 
+use UWDOEM\Framework\Filter\FilterControls;
+use UWDOEM\Framework\FilterStatement\ExcludingFilterStatement;
+
 
 class SearchFilter extends Filter {
+
+    /**
+     * @param $handle
+     * @param \UWDOEM\Framework\FilterStatement\FilterStatementInterface[] $statements
+     * @param FilterInterface|null $nextFilter
+     */
+    public function __construct($handle, array $statements, FilterInterface $nextFilter = null) {
+
+        for($i=0;$i<=5;$i++) {
+            $fieldname = FilterControls::getControl($handle, "fieldname$i");
+            $operation = FilterControls::getControl($handle, "operation$i");
+            $value = FilterControls::getControl($handle, "value$i");
+
+            if ($fieldname && $operation && $value) {
+                $statements[] = new ExcludingFilterStatement($fieldname, $operation, $value, null);
+            }
+
+        }
+
+        $this->_feedback = "";
+        foreach ($statements as $statement) {
+            $fieldname = $statement->getFieldName();
+            $value = $statement->getCriterion();
+            $operation = $statement->getCondition();
+
+            $this->_feedback .= $this->_feedback ? ", " : "";
+            $this->_feedback .= $fieldname . " " . $operation . " " . $value;
+        }
+        parent::__construct($handle, $statements, $nextFilter);
+    }
 
     /**
      * @param \UWDOEM\Framework\Row\RowInterface[] $rows
