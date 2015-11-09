4c37239227c6d5348194e689eadab9f77d548d83
Joseph Schilz <joseph@schilz.org>
2015-11-09 10:55:32 -0800

    Make TableFormBuilder use FormBuilderTrait.

--- src/table/TableFormBuilder.php
+++ src/table/TableFormBuilder.php
@@ -2,31 +2,17 @@
 
 namespace UWDOEM\Framework\Table;
 
-use UWDOEM\Framework\Form\FormAction\FormAction;
+use UWDOEM\Framework\Etc\AbstractBuilder;
 use UWDOEM\Framework\Field\Field;
-use UWDOEM\Framework\Row\RowInterface;
 use UWDOEM\Framework\Row\RowBuilder;
-use UWDOEM\Framework\Form\FormInterface;
+use UWDOEM\Framework\Form\FormBuilderTrait;
 
 
-class TableFormBuilder {
+class TableFormBuilder extends AbstractBuilder {
 
-    /** @var FormAction[] */
-    protected $_actions;
+    use FormBuilderTrait;
 
-    /** @var callable */
-    protected $_onValidFunc;
-
-    /** @var callable */
-    protected $_onInvalidFunc;
-
-    /** @var string */
-    protected $_onSuccessUrl;
-
-    /** @var array[] */
-    protected $_validators = [];
-
-    /** @var RowInterface */
+    /** @var RowBuilder */
     protected $_rowBuilder;
 
 
@@ -39,67 +25,7 @@ class TableFormBuilder {
         return $this;
     }
 
-    /**
-     * @param FormAction[] $actions
-     * @return TableFormBuilder
-     */
-    public function setActions($actions) {
-        $this->_actions = $actions;
-        return $this;
-    }
-
-    /**
-     * @param callable $onValidFunc
-     * @return TableFormBuilder
-     */
-    public function setOnValidFunc($onValidFunc) {
-        $this->_onValidFunc = $onValidFunc;
-        return $this;
-    }
-
-    /**
-     * @param callable $onInvalidFunc
-     * @return TableFormBuilder
-     */
-    public function setOnInvalidFunc($onInvalidFunc) {
-        $this->_onInvalidFunc = $onInvalidFunc;
-        return $this;
-    }
-
-    /**
-     * @param string $onSuccessRedirect
-     * @return TableFormBuilder
-     */
-    public function setOnSuccessUrl($onSuccessRedirect) {
-        $this->_onSuccessUrl = $onSuccessRedirect;
-        return $this;
-    }
-
-    /**
-     * @param string $fieldName
-     * @param callable $callable
-     * @return TableFormBuilder
-     */
-    public function addValidator($fieldName, callable $callable) {
-        if (!array_key_exists($fieldName, $this->_validators)) {
-            $this->_validators[$fieldName] = [];
-        }
-        $this->_validators[$fieldName][] = $callable;
-
-        return $this;
-    }
-
-    /**
-     * @return TableFormBuilder
-     */
-    public static function begin() {
-        return new static();
-    }
-
-    /**
-     * @return TableForm
-     */
-    public function build() {
+    protected function validateOnInvalidFunc() {
         if (!isset($this->_onInvalidFunc)) {
 
             $this->_onInvalidFunc = function (TableFormInterface $thisForm) {
@@ -114,8 +40,11 @@ class TableFormBuilder {
                 };
 
             };
-        }
 
+        }
+    }
+
+    protected function validateOnValidFunc() {
         if (!isset($this->_onValidFunc)) {
             $this->_onValidFunc = function(TableFormInterface $form) {
                 foreach ($form->getRows() as $row) {
@@ -123,27 +52,17 @@ class TableFormBuilder {
                 }
             };
         }
+    }
+    
+    /**
+     * @return TableForm
+     */
+    public function build() {
 
-        if(isset($this->_onSuccessUrl)) {
-
-            $onValidFunc = $this->_onValidFunc;
-            $url = $this->_onSuccessUrl;
-
-            $this->_onValidFunc = function(TableFormInterface $form) use ($onValidFunc, $url) {
-                if (headers_sent()) {
-                    throw new \Exception("Form success redirection cannot proceed, output has already begun.");
-                } else {
-                    header("Location: $url");
-                }
-
-                $args = func_get_args();
-                call_user_func_array($onValidFunc, $args);
-            };
-        }
-
-        if (!isset($this->_actions)) {
-            $this->_actions = [new FormAction("Submit", "POST", ".")];
-        }
+        $this->validateOnInvalidFunc();
+        $this->validateOnValidFunc();
+        $this->validateOnSuccessUrl();
+        $this->validateActions();
 
         return new TableForm(
             $this->_rowBuilder,
