0bd523a1c5e5891010eec8e70a1509af61bdeabb
Joseph Schilz <joseph@schilz.org>
2015-11-30 12:21:53 -0800

    Separate PickAForm tests from FormTests.

---
+++ tests/PickAFormTest.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once('Mocks.php');
+
+use UWDOEM\Framework\Form\FormBuilder;
+use UWDOEM\Framework\Form\FormAction\FormAction;
+use UWDOEM\Framework\PickA\PickAFormBuilder;
+use UWDOEM\Framework\Etc\StringUtils;
+
+
+class PickAFormTest extends PHPUnit_Framework_TestCase {
+
+
+    public function testPickAFormBuilding() {
+        $actions = [new FormAction("label", "method", "")];
+
+        $forms = [];
+        $labels = [];
+        for ($i = 0; $i < 3; $i++) {
+            $forms[] = FormBuilder::begin()->addFieldBearers([new MockFieldBearer])->build();
+            $labels[] = "Form $i";
+        }
+
+        $pickAForm = PickAFormBuilder::begin()
+            ->addLabel("Label Text")
+            ->addForms([
+                $labels[0] => $forms[0],
+                $labels[1] => $forms[1]
+            ])
+            ->addLabel("Label Text2")
+            ->addForms([
+                $labels[2] => $forms[2]
+            ])
+            ->setActions($actions)
+            ->build();
+
+        $this->assertEquals(array_combine($labels, $forms), $pickAForm->getSubForms());
+        $this->assertEquals($actions, array_values($pickAForm->getActions()));
+    }
+
+    /**
+     * If no form is selected, then the pick a form is not valid.
+     */
+    public function testPickAFormValidationWithNoSelection() {
+        $forms = [];
+        $labels = [];
+        for ($i = 0; $i < 2; $i++) {
+            $labels[] = "Form $i";
+            $forms[] = FormBuilder::begin()->addFieldBearers([new MockFieldBearer])->build();
+        }
+
+        $pickAForm = PickAFormBuilder::begin()
+            ->addLabel("Label Text")
+            ->addForms([
+                $labels[0] => $forms[0],
+                $labels[1] => $forms[1]
+            ])
+            ->build();
+
+        $this->assertFalse($pickAForm->isValid());
+    }
+
+    public function testPickAFormGetSelectedForm() {
+        $fieldBearers = [];
+        $forms = [];
+        $labels = [];
+        for ($i = 0; $i < 2; $i++) {
+            $fieldBearers[] = new MockFieldBearer;
+            $labels[] = "Form $i";
+            $forms[] = FormBuilder::begin()->addFieldBearers([$fieldBearers[$i]])->build();
+        }
+
+        $pickAForm = PickAFormBuilder::begin()
+            ->addLabel("Label Text")
+            ->addForms([
+                $labels[0] => $forms[0],
+                $labels[1] => $forms[1]
+            ])
+            ->build();
+
+        $selectedForm = 0;
+        $unselectedForm = 1;
+
+        $_SERVER['REQUEST_METHOD'] = "POST";
+        $_POST[$pickAForm->getHash()] = StringUtils::slugify($labels[$selectedForm]);
+
+        $this->assertEquals($forms[$selectedForm], $pickAForm->getSelectedForm());
+
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_POST = [];
+    }
+
+    /**
+     * If a form is selected, then validation passes to the selected subform.
+     */
+    public function testPickAFormValidationWithSelection() {
+        $fieldBearers = [];
+        $forms = [];
+        $labels = [];
+        for ($i = 0; $i < 2; $i++) {
+            $fieldBearers[] = new MockFieldBearer;
+            $labels[] = "Form $i";
+            $forms[] = FormBuilder::begin()->addFieldBearers([$fieldBearers[$i]])->build();
+        }
+
+        $pickAForm = PickAFormBuilder::begin()
+            ->addLabel("Label Text")
+            ->addForms([
+                $labels[0] => $forms[0],
+                $labels[1] => $forms[1]
+            ])
+            ->build();
+
+        $selectedForm = 0;
+        $unselectedForm = 1;
+
+        $_SERVER['REQUEST_METHOD'] = "POST";
+        $_POST[$pickAForm->getHash()] = StringUtils::slugify($labels[$selectedForm]);
+
+        $this->assertTrue($pickAForm->isValid());
+
+        $pickAForm->onValid();
+
+        $this->assertTrue($fieldBearers[$selectedForm]->saved);
+        $this->assertFalse($fieldBearers[$unselectedForm]->saved);
+
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_POST = [];
+    }
+}
+
