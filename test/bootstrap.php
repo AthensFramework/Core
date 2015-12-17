5aeafea3898eda65ee7ab856a14ee606e99a5eb8
Joseph Schilz <joseph@schilz.org>
2015-12-17 11:56:16 -0800

    Fix autoload order in test bootstrap.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once dirname(__FILE__) ."/../vendor/autoload.php";
+
 use UWDOEM\Encryption\Cipher;
 
 Cipher::createInstance("my_secret_passphrase");
 
 $_SERVER["REQUEST_URI"] = "http://example.com/";
-
-require_once dirname(__FILE__) ."/../vendor/autoload.php";
