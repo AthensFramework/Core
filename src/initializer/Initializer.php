ddb999d107f9689fbe67cbabd9ea75d8b9ba400e
Joseph Schilz <joseph@schilz.org>
2016-01-04 14:41:21 -0800

    Expose, satisfy /src/initializer to uwdoem/standard.

--- src/initializer/Initializer.php
+++ src/initializer/Initializer.php
@@ -8,11 +8,20 @@ use UWDOEM\Framework\Visitor\Visitor;
 use UWDOEM\Framework\Section\SectionInterface;
 use UWDOEM\Framework\Form\FormInterface;
 use UWDOEM\Framework\FieldBearer\FieldBearerInterface;
-use UWDOEM\Framework\Form\PickAFormInterface;
+use UWDOEM\Framework\PickA\PickAFormInterface;
 
+/**
+ * Class Initializer performs actions on page load to InitizableInterfaces
+ *
+ * @package UWDOEM\Framework\Initializer
+ */
 class Initializer extends Visitor
 {
 
+    /**
+     * @param mixed $child
+     * @return void
+     */
     protected function visitChild($child)
     {
         // If the given child implements the initializable interface, then initialize it
@@ -22,11 +31,19 @@ class Initializer extends Visitor
         }
     }
 
+    /**
+     * @param PageInterface $page
+     * @return void
+     */
     public function visitPage(PageInterface $page)
     {
         $this->visitChild($page->getWritable());
     }
 
+    /**
+     * @param SectionInterface $section
+     * @return void
+     */
     public function visitSection(SectionInterface $section)
     {
         foreach ($section->getWritables() as $writable) {
@@ -34,6 +51,10 @@ class Initializer extends Visitor
         }
     }
 
+    /**
+     * @param FieldBearerInterface $fieldBearer
+     * @return void
+     */
     public function visitFieldBearer(FieldBearerInterface $fieldBearer)
     {
         foreach ($fieldBearer->getFieldBearers() as $bearer) {
@@ -43,26 +64,43 @@ class Initializer extends Visitor
         $this->suffixFieldBearerFields($fieldBearer);
     }
 
+    /**
+     * @param FormInterface $form
+     * @return void
+     */
     public function visitForm(FormInterface $form)
     {
         $this->suffixFormFields($form);
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $form->isValid() ? $form->onValid(): $form->onInvalid();
         }
     }
 
+    /**
+     * @param PickAFormInterface $pickAForm
+     * @return void
+     */
     public function visitPickAForm(PickAFormInterface $pickAForm)
     {
         $this->visitForm($pickAForm);
     }
 
+    /**
+     * @param TableFormInterface $tableForm
+     * @return void
+     */
     public function visitTableForm(TableFormInterface $tableForm)
     {
-        // Suffix fields
-        // Create rows from post
+        // Suffix fields?
+        // Create rows from post?
     }
 
+    /**
+     * @param FormInterface $form
+     * @param string        $suffix
+     * @return void
+     */
     protected function suffixFormFieldsFixed(FormInterface $form, $suffix)
     {
         foreach ($form->getFieldBearer()->getFields() as $field) {
@@ -78,6 +116,11 @@ class Initializer extends Visitor
         }
     }
 
+    /**
+     * @param FormInterface $form
+     * @param integer       $countBegin
+     * @return void
+     */
     protected function suffixFormFields(FormInterface $form, $countBegin = 0)
     {
         foreach ($form->getFieldBearer()->getFields() as $field) {
@@ -96,6 +139,11 @@ class Initializer extends Visitor
         $this->suffixFieldBearerFields($form->getFieldBearer());
     }
 
+    /**
+     * @param FieldBearerInterface $fieldBearer
+     * @param integer              $countBegin
+     * @return void
+     */
     protected function suffixFieldBearerFields(FieldBearerInterface $fieldBearer, $countBegin = 0)
     {
         /** @var \UWDOEM\Framework\Field\FieldInterface $field */
