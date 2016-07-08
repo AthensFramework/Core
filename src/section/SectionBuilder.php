d9e7fdd73e1d16d82f93f282069a705bc8ff4399
Joseph Schilz <joseph@schilz.org>
2016-07-08 09:07:07 -0700

    Don't require id for section.

--- src/section/SectionBuilder.php
+++ src/section/SectionBuilder.php
@@ -2,13 +2,8 @@
 
 namespace Athens\Core\Section;
 
-use Athens\Core\Etc\AbstractBuilder;
-use Athens\Core\Etc\SafeString;
-use Athens\Core\Field\Field;
-use Athens\Core\Field\FieldBuilder;
 use Athens\Core\Writable\AbstractWritableBuilder;
 use Athens\Core\WritableBearer\WritableBearerBearerBuilderTrait;
-use Athens\Core\Writable\WritableInterface;
 
 /**
  * Class SectionBuilder
@@ -37,8 +32,6 @@ class SectionBuilder extends AbstractWritableBuilder implements SectionConstants
      */
     public function build()
     {
-        $this->validateId();
-
         $writableBearer = $this->buildWritableBearer();
 
         return new Section($this->id, $this->classes, $this->data, $writableBearer, $this->type);
