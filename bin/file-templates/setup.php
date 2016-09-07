02ef77ccdaeca0a9bb18a380d6f598e03b85e165
Joseph Schilz <joseph@schilz.org>
2016-09-07 14:58:22 -0700

    Update settings in prototype setup.

--- bin/file-templates/setup.php
+++ bin/file-templates/setup.php
@@ -12,7 +12,7 @@ require_once dirname(__FILE__) . '/settings.php';
 use Propel\Runtime\Propel;
 use Propel\Runtime\Connection\ConnectionManagerSingle;
 use Athens\CSRF\CSRF;
-use Athens\Core\Etc\Settings;
+use Athens\Core\Settings\Settings;
 use Athens\Encryption\Cipher;
 
 if (REPORT_ERRORS) {
@@ -28,8 +28,8 @@ session_start();
 CSRF::init();
 
 // Setup Framework
-Settings::addTemplateDirectory(dirname(__FILE__) ."/project-templates");
-Settings::setAcronyms(['ssn']);
+Settings::getInstance()->addTemplateDirectories(dirname(__FILE__) ."/project-templates");
+Settings::getInstance()->addAcronyms('ssn');
 Cipher::createInstance(ATHENS_ENCRYPTION_PASSWORD);
 
 
@@ -48,13 +48,13 @@ $serviceContainer->setConnectionManager(APPLICATION_NAME, $manager);
 $fullCSSFilesnames = glob(dirname(__FILE__) . "/project-assets/css/*.css");
 $relativeCSSFilesnames = str_replace(dirname(__FILE__) . "/", "", $fullCSSFilesnames);
 foreach ($relativeCSSFilesnames as $file) {
-    Settings::addProjectCSS($file);
+    Settings::getInstance()->addProjectCSS($file);
 }
 
 // Include project-specific JS
 $fullJSFilesnames = glob(dirname(__FILE__) . "/project-assets/js/*.js");
 $relativeJSFilesnames = str_replace(dirname(__FILE__) . "/", "", $fullJSFilesnames);
 foreach ($relativeJSFilesnames as $file) {
-    Settings::addProjectJS($file);
+    Settings::getInstance()->addProjectJS($file);
 }
 
