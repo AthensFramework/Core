64fd256920f96ffebb6470d7bda5de99a46fcd38
Joseph Schilz <joseph@schilz.org>
2016-09-14 15:45:12 -0700

    Transition subforms to subwritables.

---
+++ src/writable-bearer/ClassWritableBearer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Athens\Core\WritableBearer;
+
+use Propel\Runtime\ActiveRecord\ActiveRecordInterface;
+
+use Athens\Core\Etc\ORMUtils;
+use Athens\Core\Field\FieldBuilder;
+
+/**
+ * Class ClassFieldBearer Encapsulates an ActiveRecordInterface object with Framework fields.
+ *
+ * @package Athens\Core\WritableBearer
+ */
+class ClassWritableBearer extends WritableBearer
+{
+
+    /** @var ActiveRecordInterface  */
+    protected $object;
+
+    /**
+     * @param ActiveRecordInterface $object
+     * @param callable              $saveFunction
+     */
+    public function __construct(ActiveRecordInterface $object, callable $saveFunction)
+    {
+        $this->object = $object;
+
+        $fields = ORMUtils::makeFieldsFromObject($object);
+
+        $hiddenFieldNames = [];
+        foreach ($fields as $fieldName => $field) {
+            $type = $field->getType();
+            if ($type === FieldBuilder::TYPE_PRIMARY_KEY
+                || $type === FieldBuilder::TYPE_VERSION
+            ) {
+                $hiddenFieldNames[] = $fieldName;
+            }
+        }
+
+        parent::__construct($fields, [], [], $hiddenFieldNames, $saveFunction);
+    }
+}
