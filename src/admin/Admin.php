ee1d6e4177f643e23386a380f3f71b6527c425ac
Joseph Schilz <joseph@schilz.org>
2016-10-13 11:16:44 -0700

    Factor Propel out of Admin.

--- src/admin/Admin.php
+++ src/admin/Admin.php
@@ -3,6 +3,7 @@
 namespace Athens\Core\Admin;
 
 use Athens\Core\Etc\StringUtils;
+use Athens\Core\FormAction\FormActionBuilder;
 use Athens\Core\Section\SectionBuilder;
 use Athens\Core\Visitor\VisitorInterface;
 use Athens\Core\Etc\ArrayUtils;
@@ -12,11 +13,11 @@ use Athens\Core\Table\TableBuilder;
 use Athens\Core\Row\RowBuilder;
 use Athens\Core\Row\RowInterface;
 use Athens\Core\Form\FormBuilder;
-use Athens\Core\FormAction\FormAction;
 use Athens\Core\FormAction\FormActionInterface;
 use Athens\Core\Page\Page;
 use Athens\Core\WritableBearer\WritableBearerBuilder;
 use Athens\Core\ORMWrapper\ObjectWrapperInterface;
+use Athens\Core\ORMWrapper\QueryWrapperInterface;
 
 /**
  * Class ObjectManager
@@ -160,7 +161,7 @@ class Admin extends Page
      * Finds an object with the given id in this ObjectManager's query.
      *
      * @param boolean $createOnNoId
-     * @return ActiveRecordInterface
+     * @return ObjectWrapperInterface
      * @throws \Exception If object can not be found.
      */
     protected function getObjectOr404($createOnNoId = false)
@@ -168,9 +169,8 @@ class Admin extends Page
         /** @var integer|null $objectId */
         $objectId = static::getObjectId();
 
-        /** @var ActiveRecordInterface $object */
+        /** @var ObjectWrapperInterface $object */
         $object = null;
-
 
         if ($objectId !== null) {
             $object = $this->getQuery()->findOneByPrimaryKey($objectId);
@@ -202,7 +202,7 @@ class Admin extends Page
             $rows = [];
 
             /** @var string $tableName */
-            $tableName = $query->getPascalCasedObjectName();
+            $tableName = StringUtils::slugify($query->getTitleCasedObjectName());
 
             foreach ($objects as $object) {
                 /** @var string $detailurl */
@@ -272,44 +272,66 @@ class Admin extends Page
     protected function makeDetail()
     {
         /** @var boolean $idWasProvided */
-        $idWasProvided = static::getObjectId() === null;
+        $idWasProvided = static::getObjectId() !== null;
 
-        /** @var ActiveRecordInterface $object */
+        /** @var ObjectWrapperInterface $object */
         $object = $this->getObjectOr404(true);
 
         /** @var string $className */
-        $className = join('', array_slice(explode('\\', get_class($object)), -1));
+        $className = $object->getTitleCasedObjectName();
 
         /** @var string $tableName */
         $tableName = StringUtils::slugify($className);
 
         /** @var FormActionInterface $submitAction */
-        $submitAction = new FormAction(
-            [],
-            [],
-            "Submit",
-            "JS",
-            "
+        $submitAction = FormActionBuilder::begin()
+            ->setType(FormActionBuilder::TYPE_JAVASCRIPT)
+            ->setLabel('Save')
+            ->setTarget("
                 athens.ajax.AjaxSubmitForm($(this).closest('form'), function(){
-                    athens.multi_panel.closePanel(1);
-                    athens.ajax_section.loadSection('object-manager-table-$tableName');
-                });
+                        athens.multi_panel.closePanel(1);
+                        athens.ajax_section.loadSection('object-manager-table-$tableName');
+                    });
                 return false;
-            "
+            ")
+            ->build();
+
+        /** @var string $deleteUrl */
+        $deleteUrl = static::makeUrl(
+            [
+                static::MODE_FIELD => static::MODE_DELETE,
+                static::QUERY_INDEX_FIELD => $this->getQueryIndex(),
+                static::OBJECT_ID_FIELD => $object->getPrimaryKey()
+            ]
         );
 
         /** @var FormActionInterface $deleteAction */
-        $deleteAction = new FormAction(
-            [],
-            [],
-            "Delete",
-            "",
-            ""
-        );
+        $deleteAction = FormActionBuilder::begin()
+            ->setType(FormActionBuilder::TYPE_JAVASCRIPT)
+            ->setLabel('Delete')
+            ->setTarget("
+                if (confirm('Are you sure you want to delete this object?')) {
+                    athens.alert.makeAlert('Deleting object.', 'info');
+                    athens.ajax.call(
+                        '$deleteUrl',
+                        {},
+                        function() {
+                        },
+                        function() {
+                            athens.alert.makeAlert('Object deleted', 'success');
+                            athens.multi_panel.closePanel(1);
+                            athens.ajax_section.loadSection('object-manager-table-$tableName');
+                        }
+                    );
+                }
+                return false;
+            ")
+            ->build();
 
         $formBuilder = FormBuilder::begin()
             ->setId('object-manager-detail-form')
             ->addObject($object)
+            ->removeWritable($object->getPascalCasedObjectName() . '.Id')  // TODO: Make general way of getting primary key field name
             ->addAction($submitAction);
 
         if ($idWasProvided === true) {
@@ -327,11 +349,15 @@ class Admin extends Page
     }
 
     /**
-     * @return void
+     * @return WritableInterface
      */
     protected function makeDelete()
     {
         $this->getObjectOr404()->delete();
+
+        return SectionBuilder::begin()
+            ->addContent('Object deleted')
+            ->build();
     }
 
     /**
