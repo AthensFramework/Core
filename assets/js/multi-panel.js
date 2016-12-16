9ff1601c476b3de7faac105573f375efae7da766
Joseph Schilz <joseph@schilz.org>
2016-12-16 14:13:54 -0800

    Allow multiple panel buttons.

--- assets/js/multi-panel.js
+++ assets/js/multi-panel.js
@@ -1,74 +1,78 @@
 athens.multi_panel = (function () {
 
+    /**
+     * Private method for creating a slider panel.
+     *
+     * @param {number} n the depth of panel to create, eg: panel number *1*,
+     *                   panel number *2*, etc.
+     * @returns {jQuery}
+     */
+    var createPanel = function (n) {
+        var screenWidth, panelSize, panel;
+
+        screenWidth = $(window).width();
+        panelSize =  1600 - (2000 - screenWidth)*.75 - (screenWidth/10)*(n-1);
+
+        panel = $('<div class="cd-panel from-right" data-panel-for="' + n + '" style="z-index:' + n*10 + '"></div>');
+        panel.append('<div class="cd-panel-container" style="width:' + panelSize + 'px"><div class="cd-panel-content"></div></div>');
+        $('#page-container').append(panel);
+
+        $('.cd-panel').on(
+            'click',
+            function (event) {
+                if ($(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+
+                    /** The number of the panel we would like to close */
+                    var panelNum = $(this).data("panel-for");
+
+                    closePanel(panelNum);
+                    event.preventDefault();
+                }
+            }
+        );
+
+        return panel;
+    };
+
+    /**
+     * Private method for getting the panel at depth n, or creating it if
+     * it does not exist.
+     *
+     * @param n
+     * @returns {jQuery}
+     */
     var getPanel = function (n) {
         var panel = $('div.cd-panel[data-panel-for="' + n + '"]');
 
         if (panel.length === 0) {
-            panel = $('<div class="cd-panel from-right" data-panel-for="' + n + '" style="z-index:' + n*10 + '"></div>');
-            panel.append('<div class="cd-panel-container" style="width:' + sizePanel(n) + 'px"><div class="cd-panel-content"></div></div>');
-            $('#page-container').append(panel);
-            attachClosers();
+            panel = createPanel(n);
         }
 
         return panel;
     };
 
-    var sizePanel = function (n) {
-        var screenWidth = $(window).width();
-        return 1600 - (2000 - screenWidth)*.75 - (screenWidth/10)*(n-1);
-    };
-
-    var openPanel = function (n) {
-        setTimeout(function () {
-            getPanel(n).addClass('is-visible');
-
-            visPanelButtons();
-        }, 30);
-    };
-
-    var closePanel = function (n) {
-        getPanel(n).removeClass('is-visible');
-        visPanelButtons();
-    };
-
-    var createPanelButton = function (n, message) {
-        var button = getPanelButton(n);
-
-        if (button.length === 0) {
-            button = $('<div class="panel-button" data-panel-for="' + n + '" style="z-index:' + (n*10 - 5) + '"></div>');
-            button.append('<a href="#" class="cd-btn"><button type="button" style="padding:0 5px"><span class="message"></span></button></a>');
-            button.on('click', function (event) {
-                openPanel(n); event.preventDefault();});
-        }
-
-        button.find('span.message').html(message);
-
-        $('#page-container').append(button);
-
-        visPanelButtons();
-    };
-
-    var destroyPanelButton = function (n, message) {
-        getPanelButton(n).remove();
-    };
-
-
-    var getPanelButton = function (n) {
-        return  $('.panel-button[data-panel-for="' + n + '"]');
-    };
-
-    var displayPanelButton = function (n) {
-        getPanelButton(n).css('display', 'block');
+    /**
+     * Private method for displaying all panel buttons at depth n
+     *
+     * @param {number} n the depth of panel buttons to display
+     */
+    var displayPanelButtons = function (n) {
+        getPanelButtons(n).css('display', 'block');
         setTimeout(
             function () {
-                getPanelButton(n).fadeTo(500, 1);
+                getPanelButtons(n).fadeTo(500, 1);
             },
             300
         );
     };
 
-    var hidePanelButton = function (n) {
-        getPanelButton(n).fadeTo(
+    /**
+     * Private method for hiding all panel buttons at depth n
+     *
+     * @param {number} n the depth of panel buttons to hide
+     */
+    var hidePanelButtons = function (n) {
+        getPanelButtons(n).fadeTo(
             250,
             0,
             function () {
@@ -77,27 +81,112 @@ athens.multi_panel = (function () {
         );
     };
 
+    /**
+     * Private method for redetermining the visibility of all panel buttons.
+     */
     var visPanelButtons = function () {
-        var buttons = $('.panel-button');
+        var buttons = $('.panel-button-container');
 
         for (var i = 0; i < buttons.length; i++) {
             var button = $(buttons[i]);
             var panelFor = parseInt(button.data('panel-for'));
 
             if (panelFor === 1 || (getPanel(panelFor - 1).length > 0 && getPanel(panelFor - 1).hasClass('is-visible'))) {
-                displayPanelButton(panelFor);
+                displayPanelButtons(panelFor);
             } else {
-                hidePanelButton(panelFor);
+                hidePanelButtons(panelFor);
             }
         }
     };
 
-    var getTargetPanelDiv = function (n) {
-        return getPanel(n).find('.cd-panel-content');
+    /**
+     *
+     * @param n
+     * @returns {*|jQuery|HTMLElement}
+     */
+    var getPanelButtonsContainer = function (n) {
+        var buttonContainer = $('.panel-button-container[data-panel-for=' + n + ']');
+
+        if (buttonContainer.length === 0) {
+            buttonContainer = $('<div class="panel-button-container" data-panel-for="' + n + '" style="z-index:' + (n*10 - 5) + '"></div>');
+            $('#page-container').append(buttonContainer);
+        }
+
+        return buttonContainer;
     };
 
+    /**
+     * Slide the given panel open.
+     *
+     * @param {number} n
+     */
+    var openPanel = function (n) {
+        setTimeout(function () {
+            getPanel(n).addClass('is-visible');
+
+            visPanelButtons();
+        }, 30);
+    };
+
+    /**
+     * Slide the given panel closed.
+     *
+     * @param {number} n
+     */
+    var closePanel = function (n) {
+        getPanel(n).removeClass('is-visible');
+        visPanelButtons();
+    };
+
+
+    var createPanelButton = function (n, message, callback, id) {
+        var button = $('<a href="#" class="cd-btn"><button type="button"><span class="message"></span></button></a>');
+
+        if (typeof(id) !== 'undefined') {
+            button.attr('id', id);
+        }
+
+        if (typeof(callback) === 'undefined') {
+            callback = function (event) {
+                openPanel(n); event.preventDefault();
+            }
+        }
+
+        button.on('click', callback);
+        button.find('span.message').html(message);
+
+        getPanelButtonsContainer(n).append(button);
+        visPanelButtons();
+
+        return button;
+    };
+
+    /**
+     * Get all panel buttons of the given depth.
+     *
+     * @param {number} n
+     * @returns {jQuery}
+     */
+    var getPanelButtons = function (n) {
+        return  $('.panel-button-container[data-panel-for="' + n + '"]');
+    };
+
+    /**
+     * Destroy all panel buttons of the given depth.
+     * @param {number} n
+     */
+    var destroyPanelButtons = function (n) {
+        getPanelButtons(n).remove();
+    };
+
+    /**
+     * Load the panel of the given depth with the given URL.
+     *
+     * @param {number} n
+     * @param {string} targetURL
+     */
     var loadPanel = function (n, targetURL) {
-        var targetDiv = getTargetPanelDiv(n);
+        var targetDiv = getPanel(n).find('.cd-panel-content');
 
         if (typeof targetDiv !== "undefined") {
             targetDiv.html("<div class=loading-gif></div>");
@@ -117,29 +206,12 @@ athens.multi_panel = (function () {
         }
     };
 
-    var attachClosers = function () {
-        //close the lateral panel
-        $('.cd-panel').on(
-            'click',
-            function (event) {
-                if ($(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
-
-                    /** The number of the panel we would like to close */
-                    var panelNum = $(this).data("panel-for");
-
-                    closePanel(panelNum);
-                    event.preventDefault();
-                }
-            }
-        );
-    };
-
     return {
         loadPanel: loadPanel,
         openPanel: openPanel,
         closePanel: closePanel,
         createPanelButton: createPanelButton,
-        destroyPanelButton: destroyPanelButton,
+        destroyPanelButtons: destroyPanelButtons,
     };
 
 }());
